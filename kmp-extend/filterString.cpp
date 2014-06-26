33973685dd8e4eb8b2115d8309a0e036476eb8b2
liande.cai <liande.cai@qunar.com>
2014-06-26 13:24:49 +0800

    add kmp-extend

---
+++ kmp-extend/filterString.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cstring>
+#include<cstdio>
+#include<stack>
+using namespace std;
+typedef pair<int,char> pii;
+const int size =2048;
+stack<pii> s;
+string source,target;
+int next[size];
+int pos[size];
+int lenSource,lenTarget;
+
+
+void getNext()
+{
+    next[1]=0;
+    int j=0;
+    int i;
+    for(i=2;i<=lenTarget;i++)
+    {
+        while(j>0&&target[j+1]!=target[i])
+            j=next[j];
+        if(target[j+1]==target[i])
+            j++;
+        next[i]=j;
+    }
+}
+
+void kmp(){
+    int j=0;
+    for(int i=0;i<lenSource;i++)
+    {
+        while(j>0&&target[j+1]!=source[i])
+            j=next[j];
+        if(target[j+1]==source[i])
+            j++;
+        pos[i]=j;
+        s.push(make_pair(i,source[i]));
+        if(j==lenTarget)
+        {
+            for(int k=0;k<lenTarget;k++){
+                s.pop();
+            }
+            if(!s.empty()){
+                pii p = s.top();
+                j=pos[p.first];
+            }
+        }
+    }
+}
+
+void print(){
+    if(s.empty())
+        return;
+    pii tmp = s.top();
+    s.pop();
+    print();
+    cout<<tmp.second;
+
+}
+int main()
+{
+    freopen("in","r",stdin);
+    cin>>source>>target;
+    target=" "+target;
+    lenSource=source.size();
+    lenTarget=target.size()-1;
+    getNext();
+    kmp();
+    cout<<"answer:"<<endl;
+    print();
+    return 0;
+}
